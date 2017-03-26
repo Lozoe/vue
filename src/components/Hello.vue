a92b1f1ae4d3b579ef1fbbad6c75e56cdf743bb4
zhuo.liu <zhuo.liu@qunar.com>
2017-03-26 17:42:12 +0800

    udpate

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,48 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <input type="text" v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinished(item)">
+          {{item.label}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Store from '../store'
+console.log(Store)
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      items: [],
+      newItem: ''
+    }
+  },
+  watch: {
+    items: {
+      handler: function (val, oldVal) {
+        console.log(val, oldVal)
+        console.log(Store)
+        Store.save(val)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggleFinished: function (item) {
+      item.isFinished = !item.isFinished
+    },
+    addNew: function () {
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      console.log(this.newItem)
+      this.newItem = ''
     }
   }
 }
@@ -38,16 +55,20 @@ h1, h2 {
 }
 
 ul {
-  list-style-type: none;
+  /*list-style-type: none;*/
   padding: 0;
 }
 
 li {
-  display: inline-block;
+  /*display: inline-block;*/
   margin: 0 10px;
 }
 
 a {
   color: #42b983;
+}
+.finished {
+    text-decoration: underline;
+    color: green;
 }
 </style>
